89d2b28db4dab48ecc2505a822569b51abe75198
Kledi Dervishi <kledidervishi5@gmail.com>
2025-01-18 18:15:05 +0100

    updates

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,47 +1,67 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import aboutData from '../assets/about.json';
 import styles from '../styles/about.module.css';
 
-const formatContent = (content) => {
+const About = () => {
+  const token = localStorage.getItem('token');
+  const { about_container, about_section, about_title, about_content, login_link, h1_container } = styles;
+
+  const loginSection = useMemo(() => (
+    !token && (
+      <div className={login_link}>
+        <Link to='/login'>Login to get started</Link>
+      </div>
+    )
+  ), [token]);
+
+  const formattedSections = useMemo(() => {
+    const formatContent = (content) => (
+      <p>
+        {content.split('*').map((part, index) =>
+          index % 2 === 1 ? (
+            <span className='bold-text' key={index}>
+              {part}
+            </span>
+          ) : (
+            part
+          )
+        )}
+      </p>
+    );
+
+    return aboutData.aboutApp.map((section) => (
+      <div key={section.id} className={about_section}>
+        <h2 className={about_title}>{section.title}</h2>
+        <div className={about_content}>
+          {formatContent(section.content)}
+        </div>
+      </div>
+    ));
+  }, []);
+
   return (
-    <p>
-      {content.split('*').map((part, index) =>
-        index % 2 === 1 ? (
-          <span className='bold-text' key={index}>
-            {part}
-          </span>
-        ) : (
-          part
-        )
-      )}
-    </p>
+    <div className={about_container}>
+      {loginSection}
+      <div className={h1_container}>
+        <h1 className={about_title}>About Us</h1>
+      </div>
+      {formattedSections}
+    </div>
   );
 };
 
-const About = () => {
-  const token = localStorage.getItem('token');
-
-  return (
-    <div className={styles.about_container}>
-      {!token && (
-        <div className={styles.login_link}>
-          <Link to='/login'>Login to get started</Link>
-        </div>
-      )}
-      <div className={styles.h1_container}>
-        <h1 className={styles.about_title}>About Us</h1>
-      </div>
-      {aboutData.aboutApp.map((section) => (
-        <div key={section.id} className={styles.about_section}>
-          <h2 className={styles.about_title}>{section.title}</h2>
-          <div className={styles.about_content}>
-            {formatContent(section.content)}
-          </div>
-        </div>
-      ))}
-    </div>
-  );
+About.propTypes = {
+  aboutData: PropTypes.shape({
+    aboutApp: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        title: PropTypes.string.isRequired,
+        content: PropTypes.string.isRequired,
+      })
+    ).isRequired,
+  }),
 };
 
 export default About;
