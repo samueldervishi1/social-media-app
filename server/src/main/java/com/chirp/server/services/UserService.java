398b2cc0df5e75fd2c70423fb34451f2174420a8
Kledi Dervishi <kledidervishi5@gmail.com>
2025-02-08 15:58:34 +0100

    updates

--- server/src/main/java/com/chirp/server/services/UserService.java
+++ server/src/main/java/com/chirp/server/services/UserService.java
@@ -1,9 +1,8 @@
 package com.chirp.server.services;
 
+import com.chirp.server.exceptions.CustomException;
 import com.chirp.server.models.User;
 import com.chirp.server.repositories.UserRepository;
-import com.chirp.server.exceptions.CustomException;
-import com.chirp.server.utils.EncryptionUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -20,116 +19,107 @@ import java.util.regex.Pattern;
 @Service
 public class UserService {
 
-	private static final Logger logger = LoggerFactory.getLogger(UserService.class);
-	private static final SecureRandom SECURE_RANDOM = new SecureRandom();
-	private static final String DEFAULT_ROLE = "simple_account";
-	private static final String INVALID_EMAIL_FORMAT = "Invalid email format";
-	private static final String EMAIL_ALREADY_EXISTS = "Email already exists";
-	private static final String USERNAME_ALREADY_EXISTS = "Username already exists";
-	private static final String NAME_TOO_SHORT = "Full name should be at least 2 characters long";
-	private static final String INVALID_PASSWORD_FORMAT = "Password should be at least 8 characters long, including one letter, one symbol, and one number";
-	private static final Pattern EMAIL_PATTERN = Pattern.compile("^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$");
-	private static final Pattern PASSWORD_PATTERN = Pattern.compile("^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$");
+    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
+    private static final SecureRandom SECURE_RANDOM = new SecureRandom();
+    private static final String DEFAULT_ROLE = "simple_account";
+    private static final String INVALID_EMAIL_FORMAT = "Invalid email format";
+    private static final String EMAIL_ALREADY_EXISTS = "Email already exists";
+    private static final String USERNAME_ALREADY_EXISTS = "Username already exists";
+    private static final String NAME_TOO_SHORT = "Full name should be at least 2 characters long";
+    private static final String INVALID_PASSWORD_FORMAT = "Password should be at least 8 characters long, including one letter, one symbol, and one number";
+    private static final Pattern EMAIL_PATTERN = Pattern.compile("^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$");
+    private static final Pattern PASSWORD_PATTERN = Pattern.compile("^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$");
 
-	private final UserRepository userRepository;
-	private final PasswordEncoder passwordEncoder;
-	private final EncryptionUtil encryptionUtil;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-	public UserService(UserRepository userRepository , PasswordEncoder passwordEncoder , EncryptionUtil encryptionUtil) {
-		this.userRepository = userRepository;
-		this.passwordEncoder = passwordEncoder;
-		this.encryptionUtil = encryptionUtil;
-	}
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
 
-	public User createUser(User user) {
-		validateUser(user);
-		user.setEmail(encryptionUtil.encrypt(user.getEmail()));
-		user.setFullName(encryptionUtil.encrypt(user.getFullName()));
+    public User createUser(User user) {
+        validateUser(user);
+        user.setEmail(user.getEmail());
+        user.setFullName(user.getFullName());
 
-		String salt = generateSalt();
-		user.setPassword(passwordEncoder.encode(user.getPassword() + salt));
-		user.setSalt(salt);
-		user.setRole(user.getRole() == null ? DEFAULT_ROLE : user.getRole());
-		user.setAccountCreationDate(LocalDateTime.now());
+        String salt = generateSalt();
+        user.setPassword(passwordEncoder.encode(user.getPassword() + salt));
+        user.setSalt(salt);
+        user.setRole(user.getRole() == null ? DEFAULT_ROLE : user.getRole());
+        user.setAccountCreationDate(LocalDateTime.now());
 
-		logger.info("Creating user: {}" , user.getUsername());
-		return userRepository.save(user);
-	}
+        logger.info("Creating user: {}", user.getUsername());
+        return userRepository.save(user);
+    }
 
-	private String generateSalt() {
-		byte[] saltBytes = new byte[16];
-		SECURE_RANDOM.nextBytes(saltBytes);
-		return Base64.getEncoder().encodeToString(saltBytes);
-	}
+    private String generateSalt() {
+        byte[] saltBytes = new byte[16];
+        SECURE_RANDOM.nextBytes(saltBytes);
+        return Base64.getEncoder().encodeToString(saltBytes);
+    }
 
-	private void validateUser(User user) {
-		if (user == null) {
-			throw new CustomException(400 , "User cannot be null");
-		}
+    private void validateUser(User user) {
+        if (user == null) {
+            throw new CustomException(400, "User cannot be null");
+        }
 
-		if (!isValidEmail(user.getEmail())) {
-			throw new CustomException(400 , INVALID_EMAIL_FORMAT);
-		}
+        if (!isValidEmail(user.getEmail())) {
+            throw new CustomException(400, INVALID_EMAIL_FORMAT);
+        }
 
-		if (!isValidLength(user.getFullName())) {
-			throw new CustomException(400 , NAME_TOO_SHORT);
-		}
+        if (!isValidLength(user.getFullName())) {
+            throw new CustomException(400, NAME_TOO_SHORT);
+        }
 
-		if (!isValidPassword(user.getPassword())) {
-			throw new CustomException(400 , INVALID_PASSWORD_FORMAT);
-		}
+        if (!isValidPassword(user.getPassword())) {
+            throw new CustomException(400, INVALID_PASSWORD_FORMAT);
+        }
 
-		checkDuplicateCredentials(user);
-	}
+        checkDuplicateCredentials(user);
+    }
 
-	private void checkDuplicateCredentials(User user) {
-		if (userRepository.existsByEmail(user.getEmail())) {
-			logger.error("Email already exists: {}" , user.getEmail());
-			throw new CustomException(400 , EMAIL_ALREADY_EXISTS);
-		}
+    private void checkDuplicateCredentials(User user) {
+        if (userRepository.existsByEmail(user.getEmail())) {
+            logger.error("Email already exists: {}", user.getEmail());
+            throw new CustomException(400, EMAIL_ALREADY_EXISTS);
+        }
 
-		if (userRepository.existsByUsername(user.getUsername())) {
-			logger.error("Username already exists: {}" , user.getUsername());
-			throw new CustomException(400 , USERNAME_ALREADY_EXISTS);
-		}
-	}
+        if (userRepository.existsByUsername(user.getUsername())) {
+            logger.error("Username already exists: {}", user.getUsername());
+            throw new CustomException(400, USERNAME_ALREADY_EXISTS);
+        }
+    }
 
-	public User getUserInfo(String username) {
-		User user = userRepository.findByUsername(username)
-				.orElseThrow(() -> new CustomException(404 , "User not found with username: " + username));
-		user.setEmail(encryptionUtil.decrypt(user.getEmail()));
-		user.setFullName(encryptionUtil.decrypt(user.getFullName()));
+    public User getUserInfo(String username) {
+        User user = userRepository.findByUsername(username).orElseThrow(() -> new CustomException(404, "User not found with username: " + username));
+        return user;
+    }
 
-		return user;
-	}
+    public User getUserInfoById(String id) {
+        User user = userRepository.findUserById(id).orElseThrow(() -> new CustomException(404, "User not found with ID: " + id));
+        return user;
+    }
 
-	public User getUserInfoById(String id) {
-		User user = userRepository.findUserById(id)
-				.orElseThrow(() -> new CustomException(404 , "User not found with ID: " + id));
-		user.setEmail(encryptionUtil.decrypt(user.getEmail()));
-		user.setFullName(encryptionUtil.decrypt(user.getFullName()));
 
-		return user;
-	}
+    public Optional<User> findById(String userId) {
+        return userRepository.findById(userId);
+    }
 
-	public Optional<User> findById(String userId) {
-		return userRepository.findById(userId);
-	}
+    public List<User> getAllUsers() {
+        logger.info("Fetching all users from the database.");
+        return userRepository.findAll();
+    }
 
-	public List<User> getAllUsers() {
-		logger.info("Fetching all users from the database.");
-		return userRepository.findAll();
-	}
+    private boolean isValidEmail(String email) {
+        return EMAIL_PATTERN.matcher(email).matches();
+    }
 
-	private boolean isValidEmail(String email) {
-		return EMAIL_PATTERN.matcher(email).matches();
-	}
+    private boolean isValidLength(String value) {
+        return StringUtils.hasText(value) && value.length() >= 2;
+    }
 
-	private boolean isValidLength(String value) {
-		return StringUtils.hasText(value) && value.length() >= 2;
-	}
-
-	private boolean isValidPassword(String password) {
-		return PASSWORD_PATTERN.matcher(password).matches();
-	}
+    private boolean isValidPassword(String password) {
+        return PASSWORD_PATTERN.matcher(password).matches();
+    }
 }
